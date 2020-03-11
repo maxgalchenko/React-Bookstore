db393ff6b77d391895c9a6ca9ec4e307adcba300
Rainspistols <ffly@mail.ru>
2020-03-11 10:46:44 +0100

    reading state from the Redux Store

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import BookListItem from '../book-list-item';
 import './book-list.css';
+import { connect } from 'react-redux';
 
 class BookList extends Component {
-  state = {};
   render() {
     const { books } = this.props;
     return (
@@ -20,4 +20,8 @@ class BookList extends Component {
   }
 }
 
-export default BookList;
+const mapStateToProps = ({ books }) => {
+  return { books };
+};
+
+export default connect(mapStateToProps)(BookList);
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,18 +2,7 @@ import React from 'react';
 import BookList from '../book-list/book-list';
 
 const HomePage = () => {
-  return (
-    <BookList
-      books={[
-        {
-          id: 1,
-          title: 'Production-Ready Microservices',
-          author: 'Susan J. Fowler'
-        },
-        { id: 2, title: 'Release It!', author: 'Michael T. Nygard' }
-      ]}
-    />
-  );
+  return <BookList />;
 };
 
 export default HomePage;
