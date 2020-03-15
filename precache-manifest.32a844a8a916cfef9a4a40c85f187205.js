425f00ca3f3dfce1edd356db24af57129ce4535a
Rainspistols <ffly@mail.ru>
2020-03-15 13:00:44 +0100

    Updates

---
+++ precache-manifest.32a844a8a916cfef9a4a40c85f187205.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e45561874fa284ed812078a82def929f",
+    "url": "/React-Bookstore/index.html"
+  },
+  {
+    "revision": "b87cc26e4d503baea625",
+    "url": "/React-Bookstore/static/css/main.ceb7dfb8.chunk.css"
+  },
+  {
+    "revision": "8e892eac312f84434b5e",
+    "url": "/React-Bookstore/static/js/2.6f762760.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/React-Bookstore/static/js/2.6f762760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b87cc26e4d503baea625",
+    "url": "/React-Bookstore/static/js/main.29a22f6c.chunk.js"
+  },
+  {
+    "revision": "10f8595c0bd21fa10980",
+    "url": "/React-Bookstore/static/js/runtime-main.b4268681.js"
+  },
+  {
+    "revision": "2222025335d0807385c92b26ca33fca4",
+    "url": "/React-Bookstore/static/media/errorImg.22220253.png"
+  }
+]);
